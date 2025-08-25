cfeadd946af290c32b437bdf9001f7a8de987b1b
SaimanPaudel <saimanpaudel10@gmail.com>
2025-08-25 13:29:01 +1000

    updated

---
+++ Tourism Website/Controllers/FeedbackController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;  // <-- Needed for Include()
+using Tourism_Website.Models;
+
+namespace Tourism_Website.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        private readonly TourismDbContext _context = new TourismDbContext();
+
+        // GET: Feedback for a tour
+        public ActionResult Index(int tourId)
+        {
+            // Include User and Tour navigation properties
+            var feedbacks = _context.Feedbacks
+                                    .Include(f => f.User)
+                                    .Include(f => f.Tour)
+                                    .Where(f => f.TourId == tourId)
+                                    .ToList();
+            return View(feedbacks);
+        }
+
+        // GET: Leave feedback
+        public ActionResult Create(int tourId)
+        {
+            ViewBag.TourId = tourId;
+            return View();
+        }
+
+        // POST: Save feedback
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Feedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Feedbacks.Add(feedback);
+                _context.SaveChanges();
+                return RedirectToAction("Index", new { tourId = feedback.TourId });
+            }
+            return View(feedback);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Tourism Website/Models/Feedback.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tourism_Website.Models
+{
+    public class Feedback
+    {
+        public int FeedbackId { get; set; }   // Primary Key
+
+        [Required]
+        public int UserId { get; set; }       // Tourist
+
+        [Required]
+        public int TourId { get; set; }       // Which tour/package the feedback is about
+
+        [Required]
+        [Range(1, 5)]
+        public int Rating { get; set; }       // Stars (1–5)
+
+        [StringLength(500)]
+        public string Comments { get; set; }  // Feedback text
+
+        public DateTime DatePosted { get; set; } = DateTime.Now;
+    }
+}
